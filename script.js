0c99c784098a3403adf5ad9ecddbe02cefa4f88f
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-11-05 16:30:28 -0500

    force diagram inbound

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const width=500
 const height=500
 
 d3.json('airports.json').then(airports=>{
-    console.log(airports)
+    console.log(d3.extent(airports.nodes,d=>d.passengers))
+
 
     const svg = d3.select('.chart')
             .append('svg')
             .style('width', '100%')  
             .attr('viewBox', [0,0,width, height])
+
+    const circleScale=d3.scaleLinear()
+        .domain(d3.extent(airports.nodes,d=>d.passengers))
+        .range([8,30])
+    
+    const airForce = d3.forceSimulation(airports.nodes)
 })
